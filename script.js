87c4c4e6ed1351372122cdc08bd43a88c77f81ee
JeSuisMan <125714344+JeSuisMan@users.noreply.github.com>
2024-06-13 02:09:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function updateTime() {
   const newYorkTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
   const londonTime = new Date().toLocaleString("en-GB", {timeZone: "Europe/London"});
-  const tokyoTime = new Date().toLocaleString("en-JP", {timeZone: "Asia/Tokyo"});
+  const sanDiegoTime = new Date().toLocaleString("en-US", {timeZone: "America/San_Diego"});
   const sydneyTime = new Date().toLocaleString("en-AU", {timeZone: "Australia/Sydney"});
   const helsinkiTime = new Date().toLocaleString("en-FI", {timeZone: "Europe/Helsinki"});
   const newDelhiTime = new Date().toLocaleString("en-IN", {timeZone: "Asia/Kolkata"});
 
   document.getElementById("new-york-time").textContent = newYorkTime;
   document.getElementById("london-time").textContent = londonTime;
-  document.getElementById("tokyo-time").textContent = tokyoTime;
+  document.getElementById("san-diego-time").textContent = sanDiegoTime;
   document.getElementById("sydney-time").textContent = sydneyTime;
   document.getElementById("helsinki-time").textContent = helsinkiTime;
   document.getElementById("new-delhi-time").textContent = newDelhiTime;
